d9e873a9842024ab318237b7baf4f7b778a7afc9
Olavo-DIO <olavo.melo.br.DIO@gmail.com>
2021-08-25 08:20:57 -0300

    App HelloWorldUdemy Segundo commit
    pequenas correções....

--- HelloWorldUdemy/urls.py
+++ HelloWorldUdemy/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-import hello
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include('hello.urls')),
--- hello/urls.py
+++ hello/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-
+#/hello/
 urlpatterns = [
-    path('', views.hello),
-
+    path('', views.hello)
     ]
